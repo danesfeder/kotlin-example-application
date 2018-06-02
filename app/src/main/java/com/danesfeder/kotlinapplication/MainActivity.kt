95dea1f96622db4cc22999ea993540c90f100612
danesfeder <dan.nesfeder@mapbox.com>
2018-06-01 22:05:26 -0400

    Initial commit

---
+++ app/src/main/java/com/danesfeder/kotlinapplication/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.danesfeder.kotlinapplication
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val forecastList = findViewById<RecyclerView>(R.id.forecast_list)
+        forecastList.layoutManager = LinearLayoutManager(this)
+
+
+    }
+
+    fun niceToast(message: String,
+                  tag: String = MainActivity::class.java.simpleName,
+                  length: Int = Toast.LENGTH_SHORT) {
+        Toast.makeText(this, "[$tag] $message", length).show()
+    }
+}
