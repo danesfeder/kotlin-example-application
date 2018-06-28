8f9a0fcb9296b220e55a0ae7fc3d9a418af23b94
danesfeder <dan.nesfeder@mapbox.com>
2018-06-28 09:50:42 -0400

    Update adapter with new layout and load weather images

--- app/src/main/java/com/danesfeder/kotlinapplication/MainActivity.kt
+++ app/src/main/java/com/danesfeder/kotlinapplication/MainActivity.kt
@@ -4,9 +4,13 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import com.danesfeder.kotlinapplication.adapters.ForecastListAdapter
 import com.danesfeder.kotlinapplication.command.RequestForecastCommand
+import com.danesfeder.kotlinapplication.domain.Forecast
 import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.toast
 import org.jetbrains.anko.uiThread
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,7 +22,15 @@ class MainActivity : AppCompatActivity() {
 
         doAsync {
             val result = RequestForecastCommand("20003").execute()
-            uiThread { forecastList.adapter = ForecastListAdapter(result) }
+
+            uiThread {
+                forecastList.adapter = ForecastListAdapter(result,
+                        object : ForecastListAdapter.OnItemClickListener {
+                            override fun invoke(forecast: Forecast) {
+                                toast(forecast.date)
+                            }
+                        })
+            }
         }
     }
 }
---
+++ app/src/main/java/com/danesfeder/kotlinapplication/utils/ViewUtils.kt
@@ -0,0 +1,7 @@
+package com.danesfeder.kotlinapplication.utils
+
+import android.content.Context
+import android.view.View
+
+val View.ctx: Context
+    get() = context
